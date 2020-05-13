deec3a8315c9e70271061c9dddca708c5db1a70e
Ben O'neill <boneill@Bens-MacBook-Pro.local>
2020-05-12 19:48:51 -0500

    stateful additions

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,46 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+//class DicePage extends StatelessWidget {
+//  @override
+//}
+
+class DicePage extends StatefulWidget {
   @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNum = 3;
+  @override
+  //the BUILD method gets reloaded for stateless apps
   Widget build(BuildContext context) {
-    return Container();
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  leftDiceNum = 2;
+                  print('dicenum = $leftDiceNum');
+                });
+
+              },
+              child: Image.asset('images/dice$leftDiceNum.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('right button clickd');
+              },
+              child: Image.asset('images/dice2.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
